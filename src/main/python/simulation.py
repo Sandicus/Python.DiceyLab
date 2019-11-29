a34c8aa4b020957134b3d6fdb164ad35f0da3b82
MikeGray31 <michaelkgray31@gmail.com>
2019-11-29 09:48:41 -0500

    Update

--- src/main/python/simulation.py
+++ src/main/python/simulation.py
@@ -2,6 +2,16 @@ from dice import Dice
 
 
 class Simulation:
-    dice1 = Dice(2)
-    sum = dice1.tossandsum()
-    print(sum)
+
+    def __init__(self, numDice, numrolls):
+        self.numberOfDice = numDice
+        self.numberOfRolls = numrolls
+
+
+    def runSimulation(self):
+        for i in range(self.numberOfRolls):
+            dice1 = Dice(self.numberOfDice)
+            # add dice1 to bin
+
+        sum = dice1.tossandsum()
+        print(sum)

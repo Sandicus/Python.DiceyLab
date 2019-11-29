575ce93b3690a0665acb1168c3813012587e2495
MikeGray31 <michaelkgray31@gmail.com>
2019-11-29 09:59:24 -0500

    Added runSimulation function to Simulation class

--- src/main/python/simulation.py
+++ src/main/python/simulation.py
@@ -1,17 +1,15 @@
-from dice import Dice
+from src.main.python.dice import Dice
 
 
 class Simulation:
 
-    def __init__(self, numDice, numrolls):
-        self.numberOfDice = numDice
-        self.numberOfRolls = numrolls
-
+    def __init__(self, numberdice, numberrolls):
+        self.numberOfDice = numberdice
+        self.numberOfRolls = numberrolls
 
     def runSimulation(self):
         for i in range(self.numberOfRolls):
             dice1 = Dice(self.numberOfDice)
-            # add dice1 to bin
-
-        sum = dice1.tossandsum()
-        print(sum)
+            dicesum = dice1.tossandsum()
+            print(dicesum)
+            # add dicesum to bin

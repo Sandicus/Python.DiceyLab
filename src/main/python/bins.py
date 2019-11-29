b569b0f7c1449126b082320eddc60b450fcd8aac
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-11-29 10:04:57 -0500

    bins fixed, should work

--- src/main/python/bins.py
+++ src/main/python/bins.py
@@ -3,8 +3,10 @@ class Bins:
     def __init__(self, numrolls):
         self.numberOfRolls = numrolls
 
-    bin = {}
-    for x in range (self.numberOfRolls, self.numberOfRolls * 6):
-        result ={x:0}
-        bin.append(result)
+    def createBin(self):
+        bin = {}
+        for x in range (self.numberOfRolls, self.numberOfRolls * 6):
+            result ={x:0}
+            bin.append(result)
+
         return bin

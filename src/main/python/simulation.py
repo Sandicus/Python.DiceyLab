fe9ca90c91d8ba440a1f185509ea558aa26a680c
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-11-30 12:10:41 -0500

    finished stars, but need to learn how to format and do a main method

--- src/main/python/simulation.py
+++ src/main/python/simulation.py
@@ -2,6 +2,17 @@ from dice import Dice
 
 
 class Simulation:
-    dice1 = Dice(2)
-    sum = dice1.tossandsum()
-    print(sum)
+        dice1 = Dice(2)
+        bin = bins(2)
+
+        for x in range (1, 1000000):
+            sum = dice1.tossandsum()
+            bin[sum] = bin[sum] + 1
+
+
+        for x in bin:
+            print(x + "  |")
+            print(bin[x] +  "  |")
+            print(bin[x / 1000000 + "  |"])
+            for x in range(1,bin[x]):
+                print ("*")

60aff861facf5c3e201cf749df47d497d19aad26
SandyPham823 <sandydupham@gmail.com>
2019-12-02 18:14:48 -0500

    inserted a main app

---
+++ src/main/python/mainapplication.py
@@ -0,0 +1,4 @@
+from src.main.python.simulation import Simulation
+
+sim = Simulation(10,500);
+sim.runSimulation()
--- src/main/python/simulation.py
+++ src/main/python/simulation.py
@@ -10,22 +10,22 @@ class Simulation:
     bin = Bins(2)
 
 
-def __init__(self, numberdice, numberrolls):
-    self.numberOfDice = numberdice
-    self.numberOfRolls = numberrolls
+    def __init__(self, numberdice, numberrolls):
+        self.numberOfDice = numberdice
+        self.numberOfRolls = numberrolls
 
 
-def runSimulation(self):
-    for i in range(self.numberOfRolls):
-        dice1 = Dice(self.numberOfDice)
-        dicesum = dice1.tossandsum()
-        bin[dicesum] = bin[dicesum] + 1
+    def runSimulation(self):
+        for i in range(self.numberOfRolls):
+            dice1 = Dice(self.numberOfDice)
+            dicesum = dice1.tossAndSum()
+            bin[dicesum] += 1
 
 
-def printStars(bin):
-    for x in bin:
-        print(x + "  |")
-        print(bin[x] + "  |")
-        print(bin[x / 1000000 + "  |"])
-        for y in range(1, bin[x]):
-            print("*")
+    def printStars(bin):
+        for x in bin:
+            print(x + "  |")
+            print(bin[x] + "  |")
+            print(bin[x / 1000000 + "  |"])
+            for y in range(1, bin[x]):
+                print("*")
